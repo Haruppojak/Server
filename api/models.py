66016c41a3f4db145770fd9a1e4b6f45897a6f28
Seo Jiwon <wldnjstj99@gmail.com>
2024-07-21 21:55:23 +0900

    지원 모델 수정

--- api/models.py
+++ api/models.py
@@ -16,3 +16,22 @@ class TodoList(Base):
     date = Column(DateTime, nullable=False)
     todo = Column(String, nullable=False)
     check = Column(Boolean, nullable=False, default=False)
+
+class UserInfo(Base):
+    __tablename__ = "userinfo"
+
+    id = Column(Integer, primary_key=True)
+
+    userID = Column(String, nullable=False)
+    userPassword = Column(String, nullable=False)
+
+    userName = Column(String, nullable=False)
+    userBirth = Column(Date, nullable=False)  # 수정 필요
+    userEmail = Column(String, nullable=False)
+    userGender = Column(Integer, nullable=False)
+
+    userPpojakCoin = Column(Integer, nullable=False)
+    userProfileName = Column(String, nullable=False)
+    userProfileComment = Column(String, nullable=True)
+
+    # Follower, Following column 추가
