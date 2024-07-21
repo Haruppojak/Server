9e7877c27b11499a9a0f97b767ccca3b3214d595
Gromit-IT <lwc980529@gnu.ac.kr>
2024-07-21 21:49:08 +0900

    Update models.py

--- api/models.py
+++ api/models.py
@@ -1,8 +1,18 @@
 from sqlalchemy import *
 from sqlalchemy.orm import relationship
 from config.database import Base
+from datetime import datetime
 
 
 class Test(Base):
     __tablename__ = "test"
     id = Column(Integer, primary_key=True)
+
+
+class TodoList(Base):
+    __tablename__ = "todolist"
+
+    id = Column(Integer, primary_key=True, index=True)
+    date = Column(DateTime, nullable=False)
+    todo = Column(String, nullable=False)
+    check = Column(Boolean, nullable=False, default=False)
