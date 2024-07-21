54b0208aa1da658be95e36d04ad5a00cc129886c
Seo Jiwon <wldnjstj99@gmail.com>
2024-07-21 20:30:53 +0900

    UserInfo model 추가

--- api/models.py
+++ api/models.py
@@ -7,3 +7,23 @@ class Test(Base):
     __tablename__ = "test"
 
     id = Column(Integer, primary_key=True)
+
+
+class UserInfo(Base):
+    __tablename__ = "userinfo"
+
+    id = Column(Integer, primary_key=True)
+    
+    ID = Column(String, nullable=False)
+    Password = Column(String, nullable=False)
+    
+    Name = Column(String, nullable=False)
+    Birth = Column(Date, nullable=False) # 수정 필요
+    Email = Column(String, nullable=False)
+    Gender = Column(Integer, nullable=False)
+
+    PpojakCoin = Column(Integer, nullable=False)
+    ProfileName = Column(String, nullable=False)
+    ProfileComment = Column(String, nullable=True)
+
+    # Follower, Following column 추가
