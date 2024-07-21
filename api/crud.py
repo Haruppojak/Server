188768ba03e9f73ddfc14e71e1af624c39c33590
Seo Jiwon <wldnjstj99@gmail.com>
2024-07-21 23:10:10 +0900

    api 예시 추가

--- api/crud.py
+++ api/crud.py
@@ -1,4 +1,10 @@
 from sqlalchemy.orm import Session
 from datetime import datetime
-from api.models import Post, Comment
-from api.schema import CreatePostSchema, UpdatePostSchema, CreateCommentSchema
+from api.models import *
+from api.schema import *
+
+def loadTodoList(db : Session):
+    todoList = db.query(TodoList).all()
+    db.query()
+    return todoList
+
