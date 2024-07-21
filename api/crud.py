f8c499e3627438d37a148df707daa8fa938f3044
USER <codwix@naver.com>
2024-07-21 23:09:07 +0900

    Update crud.py

--- api/crud.py
+++ api/crud.py
@@ -1,4 +1,10 @@
 from sqlalchemy.orm import Session
 from datetime import datetime
-from api.models import Post, Comment
-from api.schema import CreatePostSchema, UpdatePostSchema, CreateCommentSchema
+from api.models import UserDiary
+from api.schema import CreateDiarySchema, UpdatePostSchema, CreateCommentSchema
+
+def creatediary(db: Session, diary: CreateDiarySchema):
+    diary = UserDiary(content = diary.content,
+                      date = datetime.now())
+    db.add(diary)
+    db.commit()
